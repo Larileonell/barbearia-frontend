d186e639b31fa0fc140a6f59fd46325827acb888
allanaoliveira <larissaleonel98@gmail.com>
2026-05-26 11:00:00 -0300

    feat: interceptor jwt e guard de autenticacao

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  provideHttpClient,
+  withInterceptors
+} from '@angular/common/http';
 import { routes } from './app.routes';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
